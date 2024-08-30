c79d0fd6a8603b993807f89f1cdc033c462650f4
Qkhanh2808 <phamkhanh288123@gmail.com>
2024-08-31 00:07:17 +0700

    learn deeply in map

--- src/DebugSet.java
+++ src/DebugSet.java
@@ -17,7 +17,20 @@ public class DebugSet {
         for (int i: set2){
             System.out.println(i);
         }
+        // learn all implementation of map
         Map<Integer,Integer> map= new HashMap<>();
-
+        map.put(1,2);
+        map.put(3,4);
+        map.put(2,2);
+        Map<Integer,Integer> map2= new LinkedHashMap<>();
+        map2.put(1,2);
+        map2.put(3,4);
+        map2.put(2,2);
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            System.out.println(entry);
+        }
+        for(Map.Entry<Integer,Integer> entry: map2.entrySet()){
+            System.out.println(entry);
+        }
     }
 }
